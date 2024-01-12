d0a6dfe7254fb1cb7c72cc26a3d3374fa9458874
Son Hong Do <sondh0127@gmail.com>
2024-01-12 17:02:35 +0700

    card

--- components/content/GridCard.vue
+++ components/content/GridCard.vue
@@ -7,17 +7,19 @@ defineProps<{
 </script>
 
 <template>
-  <div class="col-span-[var(--span)] flex flex-col items-start rounded-2xl bg-gray-100 dark:bg-trueGray-800 px-10 py-10"
-    :style="{ '--span': span ?? 1 }" :class="[!left ? 'justify-items-center' : 'justify-items-start']">
-    <div v-if="$slots.image" class="size-100px relative flex flex-shrink-0 items-center justify-center">
-      <Slot class="absolute inset-0 h-full w-full object-scale-down">
-        <ContentSlot :use="$slots.image" unwrap="p" />
-      </Slot>
+  <div class="col-span-[var(--span)] rounded-2xl bg-gray-100 dark:bg-trueGray-800 px-10 py-10"
+    :style="{ '--span': span ?? 1 }">
+    <div :class="[left ? '' : 'flex-center']">
+      <div v-if="$slots.image" class="size-100px relative flex flex-shrink-0">
+        <Slot class="absolute inset-0 h-full w-full object-scale-down">
+          <ContentSlot :use="$slots.image" unwrap="p" />
+        </Slot>
+      </div>
+      <div v-else-if="icon" class="size-70px flex flex-shrink-0 items-center justify-center rounded-full bg-primary/20">
+        <Icon :name="icon ?? 'i-ri:article-line'" class="h-48px w-48px text-primary" />
+      </div>
     </div>
-    <div v-else-if="icon" class="size-70px flex flex-shrink-0 items-center justify-center rounded-full bg-primary/20">
-      <Icon :name="icon ?? 'i-ri:article-line'" class="h-48px w-48px text-primary" />
-    </div>
-    <div class="mt-4 text-center font-medium text-gray-800 dark:text-gray-200">
+    <div class="mt-4 font-medium text-gray-800 dark:text-gray-200" :class="[left ? '' : 'text-center']">
       <ContentSlot :use="$slots.title" unwrap="p" />
     </div>
     <div class="mt-2" :class="[left ? '' : 'text-balance text-center']">
