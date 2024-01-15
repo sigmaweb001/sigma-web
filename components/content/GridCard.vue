c2df2f346bd8a58c56086b185a4ed631f5b68f1a
Son Hong Do <sondh0127@gmail.com>
2024-01-15 14:29:14 +0700

    subgrid

--- components/content/GridCard.vue
+++ components/content/GridCard.vue
@@ -7,7 +7,8 @@ defineProps<{
 </script>
 
 <template>
-  <div class="col-span-[var(--span)] rounded-2xl bg-gray-100 dark:bg-trueGray-800 px-10 py-10"
+  <div
+    class="col-span-[var(--span)] grid grid-row-[span_4] gap-3 grid-rows-subgrid rounded-2xl bg-gray-100 dark:bg-trueGray-800 px-10 py-10"
     :style="{ '--span': span ?? 1 }">
     <div :class="[left ? '' : 'flex-center']">
       <div v-if="$slots.image" class="size-100px relative flex flex-shrink-0">
@@ -19,13 +20,13 @@ defineProps<{
         <Icon :name="icon ?? 'i-ri:article-line'" class="h-48px w-48px text-primary" />
       </div>
     </div>
-    <div class="mt-4 font-medium text-gray-800 dark:text-gray-200" :class="[left ? '' : 'text-center']">
+    <div class="font-medium text-gray-800 dark:text-gray-200" :class="[left ? '' : 'text-center text-balance']">
       <ContentSlot :use="$slots.title" unwrap="p" />
     </div>
-    <div class="mt-2" :class="[left ? '' : 'text-balance text-center']">
+    <div :class="[left ? '' : 'text-balance text-center']">
       <ContentSlot :use="$slots.subtitle" unwrap="p" />
     </div>
-    <div class="mt-2">
+    <div v-if="$slots.default">
       <ContentSlot :use="$slots.default" unwrap="p" />
     </div>
   </div>
