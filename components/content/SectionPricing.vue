df1c20aaaf1515e1347dacb08d92cd4d75b5c35c
Son Hong Do <sondh0127@gmail.com>
2024-01-19 15:38:38 +0700

    pricing

--- components/content/SectionPricing.vue
+++ components/content/SectionPricing.vue
@@ -10,26 +10,26 @@ const appConfig = useAppConfig()
 
 useIntersectionObserver(targetRef, ([{ isIntersecting }]) => {
   full.value = isIntersecting
-}, { threshold: 1, rootMargin: appConfig.banner.enabled ? '-104px 0px 0px 0px': '-64px 0px 0px 0px' })
+}, { threshold: 1, rootMargin: appConfig.banner.enabled ? '-104px 0px 0px 0px' : '-64px 0px 0px 0px' })
 
 provide('full', full)
 </script>
 
 <template>
-  <div :class="[full ? '' : 'z-100 bg-background top-0']">
-    <section>
+  <div class="" :class="[full ? '' : '']">
+    <section :class="[full ? '' : 'opacity-0']">
       <div ref="targetRef"
         :style="{ '--col': col ?? 4, '--gap': gap ? gap + 'px' : '24px', '--padding': padding ? padding + 'px' : '0px' }"
         class="grid xl:grid-cols-[repeat(var(--col),minmax(0,1fr))] gap-[var(--gap)] p-[var(--padding)]">
 
-        <div class=""
-          :class="`container flex w-full flex-col mt-10`">
+        <div class="" :class="`container flex w-full flex-col mt-10`">
 
           <h2
             class="mt-3 max-w-2xl text-3xl font-bold leading-snug tracking-tight text-gray-800 lg:text-4xl lg:leading-tight dark:text-white">
             <ContentSlot :use="$slots.title" unwrap="p" />
           </h2>
-          <div v-if="full" class="py-2 text-start text-base prose prose-trueGray dark:prose-invert lg:text-base xl:text-base">
+          <div v-if="full"
+            class="py-2 text-start text-base prose prose-trueGray dark:prose-invert lg:text-base xl:text-base">
             <ContentSlot :use="$slots.subtitle" unwrap="p" />
           </div>
         </div>
@@ -44,5 +44,30 @@ provide('full', full)
         </div>
       </div>
     </section>
+    <div :class="[full ? 'hidden' : 'block z-100 fixed pt-5 w-screen top-0 bg-background']">
+      <div class="container mx-auto">
+        <div
+          :style="{ '--col': col ?? 4, '--gap': gap ? gap + 'px' : '24px', '--padding': padding ? padding + 'px' : '0px' }"
+          class="grid xl:grid-cols-[repeat(var(--col),minmax(0,1fr))] gap-[var(--gap)] p-[var(--padding)]">
+
+          <div class="" :class="`container flex w-full flex-col mt-10`">
+
+            <h2
+              class="mt-3 max-w-2xl text-3xl font-bold leading-snug tracking-tight text-gray-800 lg:text-4xl lg:leading-tight dark:text-white">
+              <ContentSlot :use="$slots.title" unwrap="p" />
+            </h2>
+          </div>
+          <div :style="{
+            '--col': (col ?? 4) - 1,
+            '--gap': gap ? gap + 'px' : '32px',
+            '--padding': padding ? padding + 'px' : '0px',
+            '--span': (col ?? 4) - 1
+          }"
+            class="grid col-span-[var(--span)] grid-cols-[repeat(var(--col),minmax(0,1fr))] gap-[var(--gap)] p-[var(--padding)]">
+            <ContentSlot :use="$slots.default" unwrap="p" />
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
