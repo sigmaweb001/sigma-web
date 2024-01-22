3620816e6a1f5bf5e849e1a9803c6e4c1ab0aaaf
Son Hong Do <sondh0127@gmail.com>
2024-01-22 18:07:49 +0700

    rowSpan

--- components/content/GridCard.vue
+++ components/content/GridCard.vue
@@ -3,13 +3,28 @@ defineProps<{
   span?: number
   icon: string
   left?: boolean
+  padding?: number
 }>()
+
+const slot = useSlots()
+const rowSpan = computed(() => {
+  let rowSpan = 1
+  if (slot.title)
+    rowSpan++
+  if (slot.subtitle)
+    rowSpan++
+  if (slot.default)
+    rowSpan++
+  return rowSpan
+})
 </script>
 
 <template>
   <div
-    class="col-span-[var(--span)] grid grid-row-[span_4] gap-3 grid-rows-subgrid rounded-2xl bg-gray-100 dark:bg-trueGray-800 px-10 py-10"
-    :style="{ '--span': span ?? 1 }">
+    class="col-span-[var(--span)] p-[var(--padding)] grid grid-row-[span_4] gap-3 grid-rows-subgrid rounded-2xl bg-gray-100 dark:bg-trueGray-800"
+    :style="{
+      '--span': span ?? 1, '--padding': padding ? padding + 'px' : '24px', 'grid-row': `span ${rowSpan}`
+    }">
     <div :class="[left ? '' : 'flex-center']">
       <div v-if="$slots.image" class="size-70px relative flex flex-shrink-0">
         <Slot class="absolute inset-0 h-full w-full object-scale-down">
@@ -20,7 +35,8 @@ defineProps<{
         <Icon :name="icon ?? 'i-ri:article-line'" class="h-48px w-48px text-primary" />
       </div>
     </div>
-    <div v-if="$slots.title" class="font-medium text-gray-800 dark:text-gray-200" :class="[left ? '' : 'text-center text-balance']">
+    <div v-if="$slots.title" class="font-medium text-gray-800 dark:text-gray-200"
+      :class="[left ? '' : 'text-center text-balance']">
       <ContentSlot :use="$slots.title" unwrap="p" />
     </div>
     <div v-if="$slots.subtitle" :class="[left ? '' : 'text-balance text-center']">
