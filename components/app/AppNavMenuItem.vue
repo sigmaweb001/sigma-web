4c720d79d12badeaac0d4d56b056791cb834c331
Son Hong Do <sondh0127@gmail.com>
2024-01-24 11:29:48 +0700

    clamp

--- components/app/AppNavMenuItem.vue
+++ components/app/AppNavMenuItem.vue
@@ -13,12 +13,12 @@ const props = defineProps<{
   <NavigationMenuLink as-child>
     <NuxtLink :to="_path" v-bind="$attrs"
       class="group block select-none rounded-[6px] p-8px text-gray-800 no-underline outline-none transition-colors hover:bg-primary/10 dark:text-trueGray-200 hover:text-primary">
-      <div class="flex items-center gap-3">
+      <div class="flex items-start gap-3">
         <Icon :name="icon ?? 'i-ri:article-line'"
-          class="h-10 w-10 p-2 flex-center rounded-full flex-shrink-0 bg-primary/20 text-primary">
+          class="h-10 w-10 p-2 mt-1 flex-center rounded-full flex-shrink-0 bg-primary/20 text-primary">
         </Icon>
         <div>
-          <div class="flex items-center text-base font-500">
+          <div class="flex items-center text-sm font-600">
             {{ title }}
             <span v-if="props.new" class="ml-1 rounded-lg bg-primary px-2 text-xs text-trueGray-100">
               New
@@ -27,7 +27,7 @@ const props = defineProps<{
               Coming soon
             </span>
           </div>
-          <div v-if="description" class="text-sm font-400">
+          <div v-if="description" class="text-sm line-clamp-2 mt-2px text-gray-500 dark:text-gray-400">
             {{ description }}
           </div>
         </div>
