e4d7d88651b1632a185f5e70807aa1fc305586cc
Son Hong Do <sondh0127@gmail.com>
2024-01-29 16:51:53 +0700

    fix redirect

--- components/app/AppNavMenuItem.vue
+++ components/app/AppNavMenuItem.vue
@@ -9,14 +9,15 @@ const props = defineProps<{
   redirect?: string
 }>()
 
-const target = computed(() => props.redirect?.startsWith('https://') ? '_blank' : '_self')
 const localePath = useLocalePath()
+const target = computed(() => props.redirect?.startsWith('https://') ? '_blank' : '_self')
+const _redirect = computed(() => props.redirect?.startsWith('https://') ? props.redirect : localePath(props.redirect))
 </script>
 
 <template>
   <NavigationMenuLink as-child>
     <NuxtLink
-      :to="redirect ? localePath(redirect) : localePath(_path)" :external="!!redirect" v-bind="$attrs" :target="target"
+      :to="redirect ? _redirect : localePath(_path)" :external="!!redirect" v-bind="$attrs" :target="target"
       class="group block select-none rounded-[6px] p-8px text-gray-800 no-underline outline-none transition-colors hover:bg-primary/10 dark:text-trueGray-200 hover:text-primary"
     >
       <div class="flex gap-3" :class="[description ? 'items-start' : 'items-center']">
