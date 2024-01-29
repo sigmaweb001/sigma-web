b29989ae7521e05b359a085ac859f4de087411cc
Son Hong Do <sondh0127@gmail.com>
2024-01-29 16:44:20 +0700

    fix path

--- components/app/AppNavMenuItem.vue
+++ components/app/AppNavMenuItem.vue
@@ -15,12 +15,15 @@ const localePath = useLocalePath()
 
 <template>
   <NavigationMenuLink as-child>
-    <NuxtLink :to="redirect || localePath(_path)" :external="!!redirect" v-bind="$attrs" :target="target"
-      class="group block select-none rounded-[6px] p-8px text-gray-800 no-underline outline-none transition-colors hover:bg-primary/10 dark:text-trueGray-200 hover:text-primary">
+    <NuxtLink
+      :to="localePath(redirect) || localePath(_path)" v-bind="$attrs" :target="target"
+      class="group block select-none rounded-[6px] p-8px text-gray-800 no-underline outline-none transition-colors hover:bg-primary/10 dark:text-trueGray-200 hover:text-primary"
+    >
       <div class="flex gap-3" :class="[description ? 'items-start' : 'items-center']">
-        <Icon :name="icon ?? 'i-ri:article-line'"
-          class="h-8 w-8 p-1 mt-1 flex-center rounded-full flex-shrink-0 bg-primary/20 text-primary">
-        </Icon>
+        <Icon
+          :name="icon ?? 'i-ri:article-line'"
+          class="mt-1 h-8 w-8 flex-center flex-shrink-0 rounded-full bg-primary/20 p-1 text-primary"
+        />
         <div>
           <div class="flex items-center text-sm font-600">
             {{ title }}
@@ -31,7 +34,7 @@ const localePath = useLocalePath()
               Coming soon
             </span>
           </div>
-          <div v-if="description" class="text-sm line-clamp-2 mt-2px text-gray-500 dark:text-gray-400">
+          <div v-if="description" class="line-clamp-2 mt-2px text-sm text-gray-500 dark:text-gray-400">
             {{ description }}
           </div>
         </div>
