a5f25f487674199abcdb19fca2bf3cdc0edff0ec
Son Hong Do <sondh0127@gmail.com>
2024-02-01 15:42:16 +0700

    left

--- layouts/blog.vue
+++ layouts/blog.vue
@@ -40,13 +40,13 @@ const localePath = useLocalePath()
     <div class="mx-auto container">
       <div class="max-w-screen-xl py-5 container lg:py-8">
         <h1
-          class="text-brand-primary mb-3 mt-2 text-center text-3xl font-semibold tracking-tight lg:text-4xl dark:text-white lg:leading-snug"
+          class="text-brand-primary mb-3 mt-2 text-3xl font-semibold tracking-tight lg:text-4xl dark:text-white lg:leading-snug"
         >
           {{ item?.title }}
         </h1>
 
-        <div class="flex justify-center">
-          <div v-if="tags?.length" class="mt-4 flex flex-wrap gap-2">
+        <div class="flex">
+          <div v-if="tags?.length" class="mt-2 flex flex-wrap gap-2">
             <TagItem
               v-for="(item, index) in tags" :key="index" :color="item.color"
               :to="localePath(`/resources?tag=${item.slug}`)"
@@ -56,7 +56,7 @@ const localePath = useLocalePath()
           </div>
         </div>
 
-        <div class="mt-3 flex justify-center text-gray-500 space-x-3">
+        <div class="mt-3 flex text-gray-500 space-x-3">
           <div class="flex items-center gap-3">
             <div class="relative h-10 w-10 flex-shrink-0">
               <template v-if="author.avatar">
@@ -88,7 +88,7 @@ const localePath = useLocalePath()
 
       <div class="mx-auto mt-14 max-w-screen-xl flex flex-col gap-5 px-5 md:flex-row">
         <article class="flex-1">
-          <div class="mx-auto my-3 max-w-85ch prose prose-trueGray dark:prose-invert">
+          <div class="my-3 max-w-85ch prose prose-trueGray dark:prose-invert">
             <slot />
           </div>
         </article>
