c99cabcd6a135eae36d7c603ac5f1ccd01c82c2b
congnt <congnt@gviet.vn>
2024-02-02 09:07:09 +0700

    update: file sync run

---
+++ entrypoint.sh
@@ -0,0 +1,11 @@
+git remote add target https://${INPUT_TARGET_USERNAME}:${INPUT_TARGET_TOKEN}@${INPUT_TARGET_URL#https://}
+
+case "${GITHUB_EVENT_NAME}" in
+    push)
+        git push target main
+        git push --tags target
+        ;;
+    *)
+        break
+        ;;
+esac
