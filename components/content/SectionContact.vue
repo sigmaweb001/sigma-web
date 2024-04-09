483b428040df2acbe651e4973ad692f625855b6a
giaonk <giaonk@gviet.vn>
2024-04-09 14:13:50 +0700

    fix: min length

--- components/content/SectionContact.vue
+++ components/content/SectionContact.vue
@@ -11,10 +11,10 @@ const formSchema = toTypedSchema(
   z.object({
     title: z.string(),
     name: z.string({ required_error: t('contact.required') }).min(1, { message: t('contact.required') }).max(50, { message: t('contact.max-50-characters') }),
-    email: z.string({ required_error: t('contact.required') }).email(t('contact.invalid_email')),
+    email: z.string({ required_error: t('contact.required') }).min(1, { message: t('contact.required') }).email(t('contact.invalid_email')),
     countryCode: z.string(),
-    company: z.string({ required_error: t('contact.required') }).max(50, { message: t('contact.max-50-characters') }),
-    jobTitle: z.string({ required_error: t('contact.required') }).max(50, { message: t('contact.max-50-characters') }),
+    company: z.string({ required_error: t('contact.required') }).min(1, { message: t('contact.required') }).max(50, { message: t('contact.max-50-characters') }),
+    jobTitle: z.string({ required_error: t('contact.required') }).min(1, { message: t('contact.required') }).max(50, { message: t('contact.max-50-characters') }),
     note: z.string().max(300, { message: t('contact.max-300-characters') }).optional(),
     phone: z.string({ required_error: t('contact.required') }).superRefine((val, ctx) => {
       const parsePhone = parsePhoneNumberFromString(val as string || '', form.values.countryCode || 'VN')
