42422fe86e9dd1edd3ce9d907dd7f4e4b89463c3
sigmaweb001 <156297800+sigmaweb001@users.noreply.github.com>
2024-04-17 14:44:27 +0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,6 +4,8 @@ git remote add target https://${INPUT_TARGET_USERNAME}:${INPUT_TARGET_TOKEN}@${I
 
 case "${GITHUB_EVENT_NAME}" in
     push)
+        git config --global user.email "github-action@gmail.com"
+        git config --global user.name "Github Action"
         git config pull.rebase false
         git pull target main
         git push target main
