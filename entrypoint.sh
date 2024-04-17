2d4c934c74d35f5ec9e51c02abadcab757b2bc55
sigmaweb001 <156297800+sigmaweb001@users.noreply.github.com>
2024-04-17 14:31:02 +0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,5 @@
-echo "https://${INPUT_TARGET_USERNAME}:${INPUT_TARGET_TOKEN}@${INPUT_TARGET_URL#https://}"
+
+git remote remove target
 git remote add target https://${INPUT_TARGET_USERNAME}:${INPUT_TARGET_TOKEN}@${INPUT_TARGET_URL#https://}
 
 case "${GITHUB_EVENT_NAME}" in
