ddd3f0d00f190fd0677e1b97e07e6e85809f01a6
sigmaweb001 <156297800+sigmaweb001@users.noreply.github.com>
2024-04-17 14:42:19 +0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,6 +4,7 @@ git remote add target https://${INPUT_TARGET_USERNAME}:${INPUT_TARGET_TOKEN}@${I
 
 case "${GITHUB_EVENT_NAME}" in
     push)
+        git config pull.rebase false
         git pull target main
         git push target main
         git push --tags target
