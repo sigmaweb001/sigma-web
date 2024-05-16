7cd716cb57cdb2c256b3aed1c82707120761fdc1
sigmaweb001 <156297800+sigmaweb001@users.noreply.github.com>
2024-05-16 10:38:03 +0700

    update app config

--- app.config.ts
+++ app.config.ts
@@ -19,10 +19,10 @@ export default defineAppConfig({
     title: 'Developer',
     avatar: 'author/party.png',
   }, {
-    slug: 'two',
-    name: 'Two',
-    title: 'SE',
-    avatar: 'https://unavatar.io/antfu',
+    slug: 'anhlv',
+    name: 'Luu Viet Anh',
+    title: 'Product Manager',
+    avatar: 'https://unavatar.io/github/vietkute02',
   }, {
     slug: 'linhvtm',
     name: 'linhvtm',
@@ -42,34 +42,6 @@ export default defineAppConfig({
     slug: 'tag 2',
     name: 'DRM',
     color: '#bac71a',
-  }, {
-    slug: 'test-1',
-    name: 'test-1',
-    color: '#707070',
-  }, {
-    slug: 'test-2',
-    name: 'test-2',
-    color: '#FF7C25',
-  }, {
-    slug: 'test-3',
-    name: 'test-3',
-    color: '#FF7C25',
-  }, {
-    slug: '50kitu',
-    name: '50kitu50kitu50kitu50kitu50kitu50kitu50kitu50kitu50',
-    color: '#FF0D0D',
-  }, {
-    slug: '30kitu',
-    name: '30kitu30kitu30kitu30kitu30kitu',
-    color: '#0553C9',
-  }, {
-    slug: '100kitu',
-    name: '100kitu100kitu100kitu100kitu100kitu100kitu100kitu100kitu100kitu100kitu100kitu100kitu100kitu100kitu10',
-    color: '#35c796',
-  }, {
-    slug: 'paragraph',
-    name: 'Ever wondered why some companies seem to move forward easily while others struggle to keep up? Chances are that the leading companies have figured out how to implement cross-functional teamwork.',
-    color: '#35c796',
   }, {
     slug: 'tag 3',
     name: 'SSAI',
