34f03c5a5e152b39824e39a26bd294698b4782c2
Son Hong Do <sondh0127@gmail.com>
2024-07-12 11:07:59 +0700

    feat: Add video details to play page

--- pages/play.vue
+++ pages/play.vue
@@ -1,9 +1,22 @@
 <script lang="ts" setup>
+import { withQuery } from 'ufo'
+
 definePageMeta({
   layout: false,
 })
 const isLoaded = ref(false)
 const source = computed(() => useRoute().query.source as string)
+const name = computed(() => useRoute().query.name as string)
+const tags = computed(() => useRoute().query.tags as string[])
+const category = computed(() => useRoute().query.category as string)
+
+const url = withQuery('http://localhost:7777/play', {
+  source: 'https://minio-dev.sigma.video:9000/for-test/sigma-asset/349405fa-26e2-4da2-b80b-12f08640e155/source.mp4',
+  name: 'Hello Name',
+  tags: ['tag1', 'tag2'],
+  category: 'Hello Category',
+})
+console.log('[LOG] ~ url:', url)
 
 const isHls = computed(() => source.value.includes('.m3u8'))
 const isDash = computed(() => source.value.includes('.mpd'))
@@ -32,7 +45,19 @@ else {
 <template>
   <main class="of-hidden">
     <PlayNavbar />
-    <div class="mx-auto p-4">
+    <div class="mx-auto of-hidden p-4">
+      <div class="grid grid-cols-3 mb-3">
+        <div class="col-span-2">
+          <h1 class="text-xl font-bold">
+            {{ name }}
+          </h1>
+          <div class="mt-1 flex space-x-2">
+            <span class="text-sm text-secondary-foreground"> <strong>Tags:</strong> {{ tags.join(', ') }}</span>
+            <span class="text-sm text-secondary-foreground">•</span>
+            <span class="text-sm text-secondary-foreground"> <strong>Category:</strong> {{ category }}</span>
+          </div>
+        </div>
+      </div>
       <div class="of-hidden rounded-xl">
         <PlayVideoPlayer v-if="isLoaded" :source="source" :is-dash="isDash" :is-hls="isHls" />
       </div>
